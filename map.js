a390a2061d7ced7206cda0050906f78d4092a300
devdatta <devdatta@tengshe.in>
2015-04-07 16:23:32 +0530

    Added Side Bar

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 var map;
 
-require(["esri/map","esri/SpatialReference", "esri/geometry/Extent", "dojo/domReady!"], function(Map, spatialReference, Extent) {
+require(["esri/map","esri/SpatialReference", "esri/geometry/Extent","dojo/dom" "dojo/domReady!"], function(Map, spatialReference, Extent, dom) {
 map = new Map("map", {
   basemap: "topo",  
   center:[73.1096,19.1025],
@@ -9,4 +9,15 @@ map = new Map("map", {
 map.on("load", function(event){
 	
 });
+
+//Click Events
+var Clear_btn = dom.byId("Clr_input"),add_btn = dom.byId("Add_input"),
+ on(Clear_btn, "click", function(evt){
+            console.log("inClear");
+        });
+		
+ on(add_btn, "click", function(evt){
+            console.log("inAdd");
+        });
+
 });
